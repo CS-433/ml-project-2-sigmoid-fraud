7bbb8916c62a2fe29dbfb78a6d506e2a8ad85266
daphnedequatrebarbes <92379926+daphnedequatrebarbes@users.noreply.github.com>
2023-12-12 16:39:36 +0100

    Update data_augmentation.IPYNB

--- scripts/data_augmentation.py
+++ scripts/data_augmentation.py
@@ -1,15 +1,5 @@
-import os
-import cv2 as cv
-import numpy as np
-from skimage import color  # Import color from skimage
 import albumentations as A
 
-def parse_mask_to_binary(input_mask, grayscale_threshold=0.5):
-    """
-    Function that takes an RGB mask and outputs the corresponding binary mask
-    """
-    binary_mask = np.where(color.rgb2gray(input_mask) <= grayscale_threshold, 0, 1)
-    return binary_mask
 
 TRAINING_DIR = "/content/gdrive/MyDrive/ml-project-2-sigmoid-fraud-main/data/training"
 
@@ -109,4 +99,71 @@ augmented_labels_dataset = np.concatenate([training_imgs_labels, augmented_label
 # Print the shapes of the combined dataset and labels
 print("Combined dataset shape:", augmented_dataset.shape, "Combined labels shape:", augmented_labels_dataset.shape)
 
+DEVICE = "cuda" 
+DEVICE
+
+unet_config = {
+    "model": "U-Net",
+    "patch_size": 16,
+    "n_epochs": 50,
+    "batch_size": 10,
+    "optimizer": "AdamW",
+    "loss_function": "BCELoss",
+    "learning_rate": 1e-4,
+    "prediction_transform": "identity_detach",
+    "labels_transform": "unet_reshape",
+    "scheduler": "CosineAnnealing",
+    "device": DEVICE,
+    "test_size": 0.1
+}
+
+unet = train_model(X=augmented_dataset, y=augmented_labels_dataset, config=unet_config)
+
+
+img_idx = 4
+im = cv.imread(f'/content/drive/MyDrive/ml-project-2-sigmoid-fraud-main/data/training/images/satImage_00{img_idx}.png')
+im_gt = cv.imread(f'/content/drive/MyDrive/ml-project-2-sigmoid-fraud-main/data/training/groundtruth/satImage_00{img_idx}.png')
+im_gt_gs = np.where(color.rgb2gray(im_gt) > .5, 1, 0)
+
+
+visualize_predicition_unet(img=im, gt=im_gt_gs, model=unet)
+
+def test_masks(test_directory, model, visualize_number=6):
+  
+
+
+  test_filepaths = []
+  
+  for root, dirs, files in sorted(os.walk(test_directory))[1:]: test_filepaths.append(root+'/'+files[0])
+  
+
+  output_masks = [] #Here we go baby
+  output_masks_filenames = []
+  for fdx, file in enumerate(sorted(test_filepaths)):
+
+    test_img = cv.imread(file)
+    test_img = torch.tensor(test_img[:,:,:,None].T, dtype=torch.float32).to(device=DEVICE)
+
+    mask = np.where(identity_detach(model(test_img)) > 0.5, 1, 0)[0][0]
+
+    print(f"data/test_masks/{file.split('/')[-1]}")
+    output_filename = f"data/test_masks/{file.split('/')[-1]}"
+    output_masks_filenames.append(output_filename)
+    cv.imwrite(output_filename, 255*mask.T)
+
+    output_masks.append(mask.T)
+
+  test_ids = np.random.choice(len(output_masks), size = visualize_number)
+  fig, ax = plt.subplots(visualize_number, 2, figsize = (visualize_number*3, visualize_number*3))
+
+  for i in range(visualize_number):
+    test_img = cv.imread(test_filepaths[test_ids[i]])
+    ax[i, 0].imshow(test_img)
+    ax[i, 1].imshow(output_masks[test_ids[i]])
+
+  plt.show()
+  return output_masks, output_masks_filenames
+
+x_test_dir = '/content/drive/MyDrive/ml-project-2-sigmoid-fraud-main/data/test_set_images'
+predicted_test_masks, mask_filenames = test_masks(x_test_dir, model = unet,visualize_number=10)
 
